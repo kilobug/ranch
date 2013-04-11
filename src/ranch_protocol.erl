7addb32c460576db80c801ea4f3da05cf3e80617
astutesparrow <liuzhaomin@yeah.net>
2013-04-11 21:30:45 +0800

    分析ranch_protocol模块

--- src/ranch_protocol.erl
+++ src/ranch_protocol.erl
@@ -1,21 +1,16 @@
-%% Copyright (c) 2012, Loïc Hoguin <essen@ninenines.eu>
-%%
-%% Permission to use, copy, modify, and/or distribute this software for any
-%% purpose with or without fee is hereby granted, provided that the above
-%% copyright notice and this permission notice appear in all copies.
-%%
-%% THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-%% WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-%% MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-%% ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-%% WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-%% ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-%% OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-%% @private
+%% 自定义行为模式
+%% 自定义的协议处理器需要实现这个行为模式
 -module(ranch_protocol).
 
-%% Start a new connection process for the given socket.
+%% 行为模式实现模块中在这个函数中启动新的进程处理客户端连接
+%% 典型使用如下：
+%%     Pid = spawn_link(?MODULE, init, [Ref, Socket, Transport, Opts]),
+%%     {ok, Pid}.
+%% conns sup 进程调用 start_link，启动工作进程
+
+%% 关于 callback 关键字，参考 http://my.oschina.net/astute/blog/121837
+%% 使用 callback 关键字替代 behaviour_info，极大的简化了代码，而且清晰易读
 -callback start_link(
 		Ref::any(),
 		Socket::any(),
